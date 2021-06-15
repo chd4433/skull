d49f1b7bc9bf09060284c81e6a54676d86a35264
chd4433 <chdkhj4433@naver.com>
2021-06-16 05:07:20 +0900

    210616 호준 몬스터추가

--- Client/Client/Stage1.cpp
+++ Client/Client/Stage1.cpp
@@ -30,24 +30,43 @@ HRESULT CStage1::Initialize()
 	CObj* pGroundMonster = CGrondTree::Create(1000, 495);
 	CObj* pGroundMonster2 = CGrondTree::Create(1600, 495);
 	CObj* pGroundMonster3 = CGrondTree::Create(2200, 495);
+	CObj* pGroundMonster4 = CGrondTree::Create(3000, 495);
+	CObj* pGroundMonster5 = CGrondTree::Create(4000, 495);
+	CObj* pGroundMonster6 = CGrondTree::Create(4500, 495);
 	CObj* pMovingMonster = CMovingTree::Create(1000, 495);
 	CObj* pMovingMonster2 = CMovingTree::Create(2000, 495);
+	CObj* pMovingMonster3 = CMovingTree::Create(2900, 495);
+	CObj* pMovingMonster4 = CMovingTree::Create(3200, 495);
+	CObj* pMovingMonster5 = CMovingTree::Create(2300, 495);
 	pGroundMonster->SetObjScene(SCENE_STAGE1);
 	pGroundMonster2->SetObjScene(SCENE_STAGE1);
 	pGroundMonster3->SetObjScene(SCENE_STAGE1);
+	pGroundMonster4->SetObjScene(SCENE_STAGE1);
+	pGroundMonster5->SetObjScene(SCENE_STAGE1);
+	pGroundMonster6->SetObjScene(SCENE_STAGE1);
 	pMovingMonster->SetObjScene(SCENE_STAGE1);
 	pMovingMonster2->SetObjScene(SCENE_STAGE1);
+	pMovingMonster3->SetObjScene(SCENE_STAGE1);
+	pMovingMonster4->SetObjScene(SCENE_STAGE1);
+	pMovingMonster5->SetObjScene(SCENE_STAGE1);
 	m_pObjMgr->Add(MONSTER, pMovingMonster);
 	m_pObjMgr->Add(MONSTER, pMovingMonster2);
+	m_pObjMgr->Add(MONSTER, pMovingMonster3);
+	m_pObjMgr->Add(MONSTER, pMovingMonster4);
+	m_pObjMgr->Add(MONSTER, pMovingMonster5);
 	m_pObjMgr->Add(MONSTER, pGroundMonster);
 	m_pObjMgr->Add(MONSTER, pGroundMonster2);
 	m_pObjMgr->Add(MONSTER, pGroundMonster3);
+	m_pObjMgr->Add(MONSTER, pGroundMonster4);
+	m_pObjMgr->Add(MONSTER, pGroundMonster5);
+	m_pObjMgr->Add(MONSTER, pGroundMonster6);
 	m_pObjMgr->Add(MONSTER, CRangeTree::Create(805, 313));
-	//m_pObjMgr->Add(MONSTER, CRangeTree::Create(805, 313));
+	m_pObjMgr->Add(MONSTER, CRangeTree::Create(2105, 240));
+	m_pObjMgr->Add(MONSTER, CRangeTree::Create(3075, 235));
 	
 	
 	
-	CObj* pPortal = CPortal::Create(2120, 225);
+	CObj* pPortal = CPortal::Create(4555, 460);
 	pPortal->SetObjScene(SCENE_STAGE1);
 	m_pObjMgr->Add(PORTAL, pPortal);
 	return NOERROR;
